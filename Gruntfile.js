92ad51450d318eb7b9378e5def113f3bdac8bd87
Kim Pham <ms.kimpham@gmail.com>
2015-03-29 16:55:04 -0700

    Add build and clean tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -138,7 +138,13 @@ module.exports = function (grunt) {
           src: [
             '.tmp',
             '<%= yeoman.dist %>/{,*/}*',
-            '!<%= yeoman.dist %>/.git*'
+            // '!<%= yeoman.dist %>/.git*'
+            // '<%= yeoman.dist %>/*',
+            '!<%= yeoman.dist %>/.git{,*/}*',
+            '!<%= yeoman.dist %>/Procfile',
+            '!<%= yeoman.dist %>/package.json',
+            '!<%= yeoman.dist %>/web.js',
+            '!<%= yeoman.dist %>/node_modules'
           ]
         }]
       },
@@ -388,9 +394,25 @@ module.exports = function (grunt) {
         configFile: 'test/karma.conf.js',
         singleRun: true
       }
+    },
+
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      heroku: {
+        options: {
+          remote: 'git@heroku.com:dry-chamber-1376.git',
+          branch: 'master'
+        }
+      }
     }
   });
 
+  grunt.registerTask('deploy', ['buildcontrol']);
 
   grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
     if (target === 'dist') {
